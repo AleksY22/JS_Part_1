023912965183bff06756763aedccee61dfe69169
Aleks <i.aleksey@tut.by>
2024-02-08 18:47:18 +0300

    go

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-const test = 20;
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {
+
+   },
+   actors: {
+
+   },
+   genres: [],
+   privat: false,
+};
+
+const nameLastFilm = prompt('Один из последних просмотренных фильмов?', '');
+const ratingLastFilm = prompt('Один из последних просмотренных фильмов?', '');
+personalMovieDB.movies[`${nameLastFilm}`] = ratingLastFilm;
+console.log(personalMovieDB);
