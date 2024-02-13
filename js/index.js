4658b4742535a617ad3d49c4bc64ccf6bf32a2b4
Aleks <i.aleksey@tut.by>
2024-02-13 22:00:48 +0300

    task**

--- js/index.js
+++ js/index.js
@@ -118,3 +118,34 @@ for (let i = 1; i <= data.length; i++) {
 
 console.log(result);
 */
+/*
+//v.1
+let lines = 5;
+let result = '';
+let j = 1;
+for (let i = 1; i < 7; i++) {
+   let lineStr = '';
+   lineStr = ' '.repeat(lines) + '*'.repeat(j) + '\n';
+   lines -= 1;
+   j += 2;
+   result += lineStr;
+}
+console.log(result);
+*/
+/*
+//v.2
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+   for (let j = 0; j < lines - i; j++) {
+      result += ' ';
+   }
+   for (let j = 0; j < 2 * i + 1; j++) {
+      result += '*';
+   }
+   result += '\n';
+}
+
+console.log(result);
+*/
