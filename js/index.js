14a0490c62c91f8c062a145801a1e9941d9d34b3
Aleks <i.aleksey@tut.by>
2024-03-12 21:28:13 +0300

    task**

--- js/index.js
+++ js/index.js
@@ -751,4 +751,20 @@ function unique(arr) {
    return Array.from(new Set(arr));
 }
 */
+/*
+function amountOfPages(summary) {
+   let result = '';
+   let n = 0;
 
+   for (let i = 1; i <= summary; i++) {
+      result += i;
+      if (result.length === summary) {
+         n = i;
+         break;
+      }
+   }
+   return n;
+}
+
+console.log(amountOfPages(25));
+*/
