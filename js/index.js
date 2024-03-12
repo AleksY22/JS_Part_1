b2714cfc9a301892b7f0ab8ac4868365a20a888d
Aleks <i.aleksey@tut.by>
2024-03-12 22:23:44 +0300

    task***

--- js/index.js
+++ js/index.js
@@ -752,6 +752,7 @@ function unique(arr) {
 }
 */
 /*
+//=============================********=============================
 function amountOfPages(summary) {
    let result = '';
    let n = 0;
@@ -767,4 +768,48 @@ function amountOfPages(summary) {
 }
 
 console.log(amountOfPages(25));
+*/
+/*
+// Вариант, когда строка переводится в нижний регистр до всех операций только 1 раз
+// Это должно экономить ресурсы компьютера
+function isPangram(string) {
+   string = string.toLowerCase();
+   return 'abcdefghijklmnopqrstuvwxyz'.split('').every(function (x) {
+      return string.indexOf(x) !== -1;
+   });
+}
+
+console.log(isPangram('abc'));
+
+// С другим методом и строка каждый раз преобразовывается в коллбэке
+function isPangram2(string) {
+   return 'abcdefghijklmnopqrstuvwxyz'
+      .split('')
+      .every((x) => string.toLowerCase().includes(x));
+}
+
+// При помощи цикла
+function isPangram3(str) {
+   letters: for (let c = 0; c < 26; c++) {
+      for (let i = 0; i < str.length; i++) {
+         let s = str.charCodeAt(i);
+         if (s < 65 || s > 90 && s < 97 || s > 122) continue;
+         if (s === 65 + c || s === 97 + c) continue letters;
+      }
+
+      return false;
+   }
+
+   return true;
+}
+
+// При помощи Set
+function isPangram4(string) {
+   return new Set(string.toLocaleLowerCase().replace(/[^a-z]/gi, '').split('')).size === 26;
+}
+
+// С использованием регулярных выражений
+function isPangram5(string) {
+   return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
+}
 */
